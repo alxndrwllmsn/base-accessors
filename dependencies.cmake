5e34bf5559a7bf7a0c06d105b7d7dde6edeba61b
Minh Vuong <Minh.Vuong@csiro.au>
2024-12-20 10:39:22 +1100

    AXA-3104. Do the version bump for the new release

--- dependencies.cmake
+++ dependencies.cmake
@@ -9,13 +9,13 @@
 # You can affect the build content by manipulating the "tag/branch" column. Should be something that git expects for that 
 # repo.
 #     Identifier                   tag/branch       cache  type      description                        force it
-set ( ASKAP_CMAKE_TAG              tags/1.5.0      CACHE  STRING    "askap.cmake tools"                FORCE )
+set ( ASKAP_CMAKE_TAG              tags/1.6.0      CACHE  STRING    "askap.cmake tools"                FORCE )
 set ( ASKAP_DEV_TAG                tags/2.13.0     CACHE  STRING    "askap dev tools"                  FORCE )
-set ( LOFAR_COMMON_TAG             f35117f80d12c9bd4a363c23fb00c8e0c49b1cee    CACHE  STRING    "lofar-common version"             FORCE )
+set ( LOFAR_COMMON_TAG             tags/1.3.0      CACHE  STRING    "lofar-common version"             FORCE )
 set ( LOFAR_BLOB_TAG               tags/1.3.1      CACHE  STRING    "lofar-blob version"               FORCE )
-set ( BASE_ASKAP_TAG               ffd9370a6e5ebc5e3f07e2383bca2df431dd8269    CACHE  STRING    "base-askap version"               FORCE )
-set ( BASE_LOGFILTERS_TAG          57941abe0e9f13975bf4187a3ba7223bb49477e5    CACHE  STRING    "base-logfilters version"          FORCE )
-set ( BASE_IMAGEMATH_TAG           9ce81c9021c013eb1dbdce83fe6e94a6228d68b9    CACHE  STRING    "base-imagemath version"           FORCE )
-set ( BASE_ASKAPPARALLEL_TAG       1ccc9663cf58a71007db5dc48e4ad3beb0a4d419    CACHE  STRING    "base-askapparallel version"       FORCE )
-set ( BASE_SCIMATH_TAG             feb949247c760972ae3c7a0936bb7869ee3ebc35    CACHE  STRING    "base-scimath version"             FORCE )
+set ( BASE_ASKAP_TAG               tags/1.8.0      CACHE  STRING    "base-askap version"               FORCE )
+set ( BASE_LOGFILTERS_TAG          tags/1.5.0      CACHE  STRING    "base-logfilters version"          FORCE )
+set ( BASE_IMAGEMATH_TAG           tags/1.15.0     CACHE  STRING    "base-imagemath version"           FORCE )
+set ( BASE_ASKAPPARALLEL_TAG       tags/1.10.0     CACHE  STRING    "base-askapparallel version"       FORCE )
+set ( BASE_SCIMATH_TAG             tags/1.15.0     CACHE  STRING    "base-scimath version"             FORCE )
 # TOS related repos are not versioned yet, so pinned this build with commit hash values.
